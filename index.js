e00e8e9ddb2720f2df15f3ed9d6f6dcb2b46efae
Ethan-liao <ethan-liao@outlook.com>
2018-09-13 00:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyAppendDriver(fname,lname, age){
   drivers.push(fname)
   drivers.push(lname)
   drivers.push(age)
-  
+  return "hello"
 }
 
 destructivelyAppendDriver('john','doe', 'age')
